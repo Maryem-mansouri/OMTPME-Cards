cd9006c858c295e74cca93dd995eecc1e0a64464
Maryem El Mansouri <maryemelmansouri21@gmail.com>
2026-06-02 20:45:45 +0100

    migrate to huggingface

--- Carte.py
+++ Carte.py
@@ -1263,4 +1263,4 @@ def logout(n_clicks):
 #    app.run(debug=False, host="0.0.0.0", port=8050)
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8050)))
+    app.run(debug=False, host="0.0.0.0", port=7860)
